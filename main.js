ab8bfb522fa254fb6ba162de6d6df4bd05fca939
Formaline-shymy <52758074+Formaline-shymy@users.noreply.github.com>
2020-01-07 13:13:24 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let slider = new Slider();
-
+let canvas = new Canvas();
 let map = new Map("https://api.jcdecaux.com/vls/v1/stations?contract=Mulhouse&apiKey=41dd5bacc66f902914ec4bc2c840ef00bcd1b57e")
 
 	
--- map.js
+++ map.js
@@ -10,23 +10,78 @@ class Map{
 	accessToken: 'pk.eyJ1IjoiZm9ybWFsaW5lIiwiYSI6ImNrNGUwemd0dDA4NHYzbmxoMTAxb2lqZG8ifQ.KRrk14Oh8FHzS9NxUickFw',
     }).addTo(this.mymap);
     
-    this.url = url;   
+    this.url = url;  
     this.displayMap(); 
 	this.station = stations;
-	this.marker = markers;
-  }// - -end of constructor
+	this.marker = markers;	
+	this.init();
 	
-	  displayMap() {
-        ajaxGet(this.url, (reponse) => {
-            let results = JSON.parse(reponse);
-
-            for (let i = 0; i < results.length; i++) {
+  }// - -end of constructor
+  
+  
+     init(){
+	     this.redIcon = new L.Icon({
+            iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+            iconSize: [23, 39],
+            iconAnchor: [12, 39],
+           	shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            shadowSize: [41, 41],
+          });
+           this.greenIcon = new L.Icon({
+            iconUrl: 'Images/markergreen.png',
+            iconSize: [23, 39],
+            iconAnchor: [12, 39],
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            shadowSize: [41, 41],
+          });
+		  this.blackIcon = new L.Icon({
+			iconUrl :'Images/markerblack.png',
+            iconSize: [23, 39],
+            iconAnchor: [12, 39],
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            shadowSize: [41, 41],
+          });
+		  this.orangeIcon = new L.Icon({
+			iconUrl :'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png',
+            iconSize: [23, 39],
+            iconAnchor: [12, 39],
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            shadowSize: [41, 41],
+          });
+		  
+		
+	 } //--end init   
+	 /*for (let i = 0; i < results.length; i++) {
                 this.station = results[i];
                 this.marker = L.marker([this.station.position.lat,this.station.position.lng]);
                 this.marker.addTo(this.mymap);
-				console.log(this.station)
+				console.log(this.station) }*/
+       
+	  displayMap() {
+        ajaxGet(this.url, (reponse) => {
+            let results = JSON.parse(reponse);
+				
+         results.forEach(station => {
+                this.latLng = [station.position.lat, station.position.lng];
+                if(station.status === "CLOSED"){
+                    this.marker = L.marker([station.position.lat,station.position.lng], {icon: this.blackIcon});
+                    this.marker.addTo(this.mymap);
+                } 
+				else if(station.status === "OPEN" && station.available_bikes ===0){
+                    this.marker = L.marker([station.position.lat,station.position.lng], {icon: this.redIcon});
+                    this.marker.addTo(this.mymap);
                 }
-        })
-    } //-- end of dispaly
-
+				else if(station.status === "OPEN" &&  station.available_bikes <=3){
+                    this.marker = L.marker([station.position.lat,station.position.lng], {icon: this.orangeIcon});
+                    this.marker.addTo(this.mymap);
+                }
+                else {
+                    this.marker = L.marker([station.position.lat,station.position.lng] ,{icon: this.greenIcon});
+                    this.marker.addTo(this.mymap);
+                }
+				
+		 })//--end of results.forEach
+		})//-- end of ajaxGet
+		 }//--end of displayMap
+		 
 }//--end of class
